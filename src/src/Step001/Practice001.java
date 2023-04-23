f60436d05401e8a83e0d1e9df9cbbc275bb334bc
kyu05 <kyu05017@naver.com>
2023-04-23 13:22:27 +0900

    백준 연습 커밋

---
+++ src/src/Step001/Practice001.java
@@ -0,0 +1,14 @@
+package Step001;
+
+
+/**
+ * @Author: kyu05017
+ * @Date: 2023/04/23
+ * @Description: 백준 단계별 연습문제 풀이 1번
+ * @Ttitle : HelloWorld
+ */
+public class Practice001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
