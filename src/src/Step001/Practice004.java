4abf12ce3baa7bd2a3a09bc115a9b8cf7960c0f6
kyu05 <kyu05017@naver.com>
2023-04-23 17:30:16 +0900

    백준 연습 커밋

---
+++ src/src/Step001/Practice004.java
@@ -0,0 +1,24 @@
+package Step001;
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+/**
+ * @Author: kyu05017
+ * @Date: 2023/04/23
+ * @Description: 백준 단계별 연습문제 풀이 4번
+ * @Ttitle : AXB
+ */
+
+public class Practice004 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int firstNum = sc.nextInt();
+        int secondNum = sc.nextInt();
+
+        System.out.println(firstNum*secondNum);
+    }
+}
---
+++ src/src/Step001/Practice006.java
@@ -0,0 +1,27 @@
+package Step001;
+
+import java.util.Scanner;
+
+/**
+ * @Author: kyu05017
+ * @Date: 2023/04/23
+ * @Description: 백준 단계별 연습문제 풀이 6번
+ * @Ttitle : 사칙연산
+ */
+
+public class Practice006 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int firstNum = sc.nextInt();
+        int secondNum = sc.nextInt();
+
+        System.out.println(firstNum+secondNum);
+        System.out.println(firstNum-secondNum);
+        System.out.println(firstNum*secondNum);
+        System.out.println(firstNum/secondNum);
+        System.out.println(firstNum%secondNum);
+    }
+}
