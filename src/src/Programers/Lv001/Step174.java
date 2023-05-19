1f443fa52f74f6619c36edbb579df7280885d8e0
kyu05 <kyu05017@naver.com>
2023-05-19 23:48:19 +0900

    알고리즘 공부 및 프로그래머스 풀이

---
+++ src/src/Programers/Lv001/Step174.java
@@ -0,0 +1,27 @@
+package Programers.Lv001;
+
+/**
+ * @Author: kyu05017
+ * @Date: 2023/05/19
+ * @Description: 프로그래머스 Lv01 174단게
+ * @Ttitle : 배열 원소의 길이
+ */
+
+public class Step174 {
+	public static void main(String[] args) {
+
+		System.out.println(
+				(solution(new String[] {"We", "are", "the", "world!"}))
+		);
+	}
+
+	public static int[] solution(String[] str) {
+		int[] answer = new int[str.length];
+		int i =0;
+		for(String temp : str){
+			answer[i] = temp.length();
+			i++;
+		}
+		return answer;
+	}
+}
