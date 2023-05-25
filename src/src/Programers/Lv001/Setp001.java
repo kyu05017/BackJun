39341bbaaa4b2e5934efc8ce87810d6116105de5
kyu05 <kyu05017@naver.com>
2023-05-25 20:38:34 +0900

    알고리즘 공부 및 프로그래머스 풀이

--- src/src/Programers/Lv001/Setp001.java
+++ src/src/Programers/Lv001/Setp001.java
@@ -11,16 +11,27 @@ import java.util.Arrays;
 
 
 public class Setp001 {
-	public static String[] solution(String[] players, String[] callings) {
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(solution(new String[]{"mumu", "soe", "poe", "kai", "mine"},new String[]{"kai", "kai", "mine", "mine"})));
+	}
 
+	public static String[] solution(String[] players, String[] callings) {
+		// 해당 풀이는 시간초과
 		String[] answer = new String[players.length];
 		System.arraycopy(players, 0, answer, 0, answer.length);
-		System.out.println(Arrays.toString(answer));
+
 		for(int i = 0; i < callings.length; i++) {
 			for(int j = 0; j < players.length; j++) {
-
+				if(callings[i].equals(answer[j])) {
+					String temp = answer[j - 1];
+					answer[j - 1] = answer[j];
+					answer[j] = temp;
+				}else {
+					continue;
+				}
 			}
 		}
+		// 해당 풀이는 시간초과
 		return answer;
 	}
 }
