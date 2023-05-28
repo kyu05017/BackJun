ae384ef2b855c32f8ce7398ee2f043809e2c3d16
kyu05 <kyu05017@naver.com>
2023-05-28 14:58:04 +0900

    알고리즘 공부 및 프로그래머스 풀이

--- src/src/Programers/Lv001/Setp001.java
+++ src/src/Programers/Lv001/Setp001.java
@@ -1,6 +1,6 @@
 package Programers.Lv001;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * @Author: kyu05017
@@ -12,10 +12,10 @@ import java.util.Arrays;
 
 public class Setp001 {
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(solution(new String[]{"mumu", "soe", "poe", "kai", "mine"},new String[]{"kai", "kai", "mine", "mine"})));
+		System.out.println(Arrays.toString(solution2(new String[]{"mumu", "soe", "poe", "kai", "mine"},new String[]{"kai", "kai", "mine", "mine"})));
 	}
 
-	public static String[] solution(String[] players, String[] callings) {
+	public static String[] solution1(String[] players, String[] callings) {
 		// 해당 풀이는 시간초과
 		String[] answer = new String[players.length];
 		System.arraycopy(players, 0, answer, 0, answer.length);
@@ -26,12 +26,27 @@ public class Setp001 {
 					String temp = answer[j - 1];
 					answer[j - 1] = answer[j];
 					answer[j] = temp;
-				}else {
-					continue;
 				}
 			}
 		}
 		// 해당 풀이는 시간초과
 		return answer;
+	}
+
+	public static String[] solution2(String[] players, String[] callings){
+		Map<String, Integer> playersMap = new HashMap<>();
+		for(int i=0; i<players.length; i++) playersMap.put(players[i], (i+1));
+
+		for(int i=0; i<callings.length; i++) {
+			String temp = "";
+			temp = players[playersMap.get(callings[i])-2];
+			players[playersMap.get(callings[i])-2] = players[playersMap.get(callings[i])-1];
+			players[playersMap.get(callings[i])-1] = temp;
+
+			playersMap.put(callings[i], playersMap.get(callings[i])-1);
+			playersMap.put(temp, playersMap.get(temp)+1);
+
+		}
+		return players;
 	}
 }
